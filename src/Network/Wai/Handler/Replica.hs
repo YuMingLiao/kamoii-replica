fa674994fb52abd7a7e435210b3c00c56a02a4aa
YuMingLiao <dycan0716@gmail.com>
2025-06-10 04:23:03 +0000

    clean

--- src/Network/Wai/Handler/Replica.hs
+++ src/Network/Wai/Handler/Replica.hs
@@ -304,7 +304,6 @@ attachSessionToWebsocket conn ses = withWorker eventLoop frameLoop
     eventLoop :: IO Void
     eventLoop = forever $ do
         ev' <- (A.decode . (\bs -> trace (show bs) bs)) <$> receiveData conn
-        traceM $ show ev'
         ev <- maybe (throwIO IllformedData) pure ev'
         atomically $ S.feedEvent ses ev
 
